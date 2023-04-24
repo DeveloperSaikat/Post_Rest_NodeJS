0f3e9ebad7abcdaad9695514883338c0927b20c7
DeveloperSaikat <saikatk594@gmail.com>
2023-04-24 19:05:31 +0530

    jwt code added

--- routes/feeds.js
+++ routes/feeds.js
@@ -2,28 +2,30 @@ const express = require('express');
 const { body } = require('express-validator');
 
 const feedController = require('../controllers/feeds');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 //GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 //POST /feed/post
 router.post('/post', 
+isAuth,
 [body('title').trim().isLength({min: 5}),
 body('content').trim().isLength({min: 5})],
 feedController.createPost);
 
 //GET a single post
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId', isAuth, feedController.getPost);
 
 //UPDATE a post
-router.put('/post/:postId', 
+router.put('/post/:postId', isAuth,
 [body('title').trim().isLength({min: 5}),
 body('content').trim().isLength({min: 5})],
 feedController.updatePost);
 
 //DELETE a post 
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId',isAuth , feedController.deletePost);
 
 module.exports = router;
